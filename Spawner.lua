76817468b99b959632bfd728017413e6185443a4
FlamingArr <53872662+FlamingArr@users.noreply.github.com>
2020-10-24 15:03:41 +0530

    Add files via upload

---
+++ Spawner.lua
@@ -0,0 +1,44 @@
+require "Enemy"
+local rand = love.math.random
+
+function Spawner()
+	spawner = {}
+	
+	spawner.enemies = {} --All the enemies
+	spawner.o2s = {} --All the oxygen cylinders
+	
+	function spawner:draw()
+		for _, v in ipairs(self.enemies) do
+			v:draw()
+		end
+		for _, v in ipairs(self.o2s) do
+			v:draw()
+		end
+	end
+	
+	function spawner:update(dt)
+		for i = #self.enemies, 1, -1 do
+			local e = self.enemies[i]
+			e:takeDamage(1)
+			e:update(dt)
+			if e:isDead() then
+				table.remove(self.enemies, i)
+			end
+		end
+		for _, v in ipairs(self.o2s) do
+			v:update(dt)
+		end
+		
+		local t = math.abs(math.sin(love.timer.getTime()))
+		
+		if t > .9 then
+			table.insert(self.enemies, Enemy())
+		end
+	end
+	
+	function spawner:target(x, y)
+		for _, v in ipairs(self.enemies) do
+			v:target(x, y)
+		end
+	end 
+end
